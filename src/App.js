b0bbd1f23033b7b5b3ebdb256d3cf6209482487d
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2018-09-19 17:37:58 +0300

    Feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to HELL! </h1>
+          <h1 className="App-title">This is feature branch</h1>
         </header>
         <p className="App-intro">
           PREVED 
